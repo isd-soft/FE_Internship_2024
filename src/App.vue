85409b3ac1400cb9500e24edbf97ada3700d4c7b
AlmightyCrickityCrick <s02linamd@gmail.com>
2024-04-01 21:07:23 +0300

    Rebase complete

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+
+</script>
+
+<template>
+  <header>
+  </header>
+
+  <button></button>
+  <!-- <RouterView /> -->
+
+  <footer>
+  </footer>
+</template>
+
+<style lang="scss">
+
+</style>
