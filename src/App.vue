e2314dba98afb3934a1122cbcc950f67a77e2d23
Stanislav Prutean <stanislav.prutean@iis.utm.md>
2024-04-02 15:16:22 +0300

    add footer

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-
+import Footer from './components/PageFooter.vue'
 </script>
 
 <template>
   <header>
   </header>
   <!-- <RouterView /> -->
-
-  <footer>
-  </footer>
+  <Footer>
+  </Footer>
 </template>
 
 <style lang="scss">
