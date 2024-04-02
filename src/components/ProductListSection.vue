5ebad107660da3b6930f3d2d7f3c064123375ac4
trickolor <nikita.albu002@gmail.com>
2024-04-02 16:11:12 +0100

    wip, trying to implement generic list and set the product list section

--- src/components/ProductListSection.vue
+++ src/components/ProductListSection.vue
@@ -2,8 +2,29 @@
     <section class="main__section section product-list-section">
         <div class="product-list-section__container container">
             <h2 class="product-list-section__title">
-                Our Products 
+                Our Products
             </h2>
+
+            <GenericList :items="productList" :listTag="'ul'" :isVertical="false">
+                <ProductCard></ProductCard>
+            </GenericList>
         </div>
     </section>
 </template>
+
+<script setup>
+import GenericList from './generics/GenericList.vue';
+import ProductCard from './ProductCard.vue';
+
+const productList = [
+    { imageSrc: 'https://via.placeholder.com/381x480/CCCCCC/FFFFFF?text=Placeholder+Image', title: 'Syltherine', description: 'Stylish cafe chair', price: 2500000, oldPrice: 3500000, productType: 'stock' },
+    { imageSrc: 'https://via.placeholder.com/381x480/CCCCCC/FFFFFF?text=Placeholder+Image', title: 'Syltherine', description: 'Stylish cafe chair', price: 2500000, oldPrice: 3500000, productType: 'stock' },
+    { imageSrc: 'https://via.placeholder.com/381x480/CCCCCC/FFFFFF?text=Placeholder+Image', title: 'Syltherine', description: 'Stylish cafe chair', price: 2500000, oldPrice: 3500000, productType: 'stock' },
+    { imageSrc: 'https://via.placeholder.com/381x480/CCCCCC/FFFFFF?text=Placeholder+Image', title: 'Syltherine', description: 'Stylish cafe chair', price: 2500000, oldPrice: 3500000, productType: 'stock' },
+    { imageSrc: 'https://via.placeholder.com/381x480/CCCCCC/FFFFFF?text=Placeholder+Image', title: 'Syltherine', description: 'Stylish cafe chair', price: 2500000, oldPrice: 3500000, productType: 'stock' },
+    { imageSrc: 'https://via.placeholder.com/381x480/CCCCCC/FFFFFF?text=Placeholder+Image', title: 'Syltherine', description: 'Stylish cafe chair', price: 2500000, oldPrice: 3500000, productType: 'stock' },
+    { imageSrc: 'https://via.placeholder.com/381x480/CCCCCC/FFFFFF?text=Placeholder+Image', title: 'Syltherine', description: 'Stylish cafe chair', price: 2500000, oldPrice: 3500000, productType: 'stock' },
+    { imageSrc: 'https://via.placeholder.com/381x480/CCCCCC/FFFFFF?text=Placeholder+Image', title: 'Syltherine', description: 'Stylish cafe chair', price: 2500000, oldPrice: 3500000, productType: 'stock' },
+]
+
+</script>
