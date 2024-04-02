f494cab08211e2902d777f513728bc30e251342d
Stanislav Prutean <stanislav.prutean@iis.utm.md>
2024-04-02 15:18:21 +0300

    solve pull conflict

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,11 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import Footer from './components/PageFooter.vue'
 </script>
 
 <template>
   <header>
   </header>
   <!-- <RouterView /> -->
-  <Footer>
-  </Footer>
 </template>
 
 <style lang="scss">
