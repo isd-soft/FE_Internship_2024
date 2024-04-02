55c51b6a5d0118ca12fdcfcce8c66d85e9749a0c
YourBestDream <ferari100w3@gmail.com>
2024-04-02 12:47:03 +0300

    CHnegd the font of Furniro

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -37,7 +37,6 @@ export default {
 </template>
 
 <style scoped>
-@import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
 @import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap');
 
 .navigation {
@@ -45,7 +44,6 @@ export default {
     height: 100px;
     max-width: 1440px;
     flex-direction: row;
-    font-family: "Poppins", sans-serif;
     justify-content: space-around;
     margin: 0 auto;
     align-items: center;
