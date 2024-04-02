8098e95f973e78c5aaa996ec598015ed6ef0f9f3
Stanislav Prutean <stanislav.prutean@iis.utm.md>
2024-04-02 18:01:12 +0300

    add advantages component ,fix footer

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,14 @@
 import { RouterLink, RouterView } from 'vue-router'
 import Navbar from '@/components/Navbar.vue'
 import Footer from "@/components/PageFooter.vue"
-import Advantages from "@/components/Advantages.vue"
 </script>
 
 <template>
   <header>
     <Navbar />
   </header>
-  <!-- <RouterView /> -->
-
-<Footer/>
+  <RouterView /> 
+  <Footer/>
 </template>
 
 <style lang="scss">
