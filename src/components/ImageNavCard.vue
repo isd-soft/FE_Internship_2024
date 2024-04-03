a2d10af86598a66cefaa595004086000b3786bfe
Stanislav Prutean <stanislav.prutean@iis.utm.md>
2024-04-03 10:39:50 +0300

    add ImageNavCard

---
+++ src/components/ImageNavCard.vue
@@ -0,0 +1,33 @@
+<script setup>
+    import Logo from '@/assets/icons/Furniro_logo.svg'
+    import GenericLink from '@/components/generics/GenericLink.vue'
+    import { defineProps } from '@vue/runtime-core'
+
+const props = defineProps({
+    navName:String
+});
+</script>
+
+<template>
+    <div class="navCard">
+        <div class="navigation__logo">
+            <generic-link href="#" container-class="navigation__logo-link"  :text-first="false">
+            <Logo />
+            </generic-link>
+            <p>{{ props.navName }}</p>
+            <div>
+                <generic-link href="#" container-class="navigation__logo-link" text-content="Home" :text-first="false"/>
+                <span>></span>
+                <span>{{ props.navName }}</span>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+    .navCard{
+        background-image: url(../../public/background_image_card.png);
+        width: 100vw;
+        height: 40vh;
+    }
+</style>
--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,1 +1,15 @@
-<template></template>
+<script setup>
+    import Advantages from "@/components/Advantages.vue"
+    import ImageNavCard from "@/components/ImageNavCard.vue"
+</script>
+
+<template>
+    <div>
+        <ImageNavCard namName="Contact"/>
+        <Advantages/>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
