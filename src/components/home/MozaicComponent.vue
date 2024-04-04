e8ed50831dc3e86cc031398657843b079b9fa04a
Pascal Adrian <aa4033339@icloud.com>
2024-04-04 15:53:39 +0300

    add swiper for gallery

--- src/components/home/MozaicComponent.vue
+++ src/components/home/MozaicComponent.vue
@@ -1,12 +1,16 @@
 <script setup>
 import { mozaicImagesUrls } from '../../utils/mozaicImagesUrls'
 import { getUrlFromString } from '../../utils/getUrlFromString'
+import { Swiper, SwiperSlide } from 'swiper/vue'
+import 'swiper/css'
+import { EffectCards } from 'swiper/modules'
+import 'swiper/css/effect-cards'
 
 const urls = mozaicImagesUrls
 </script>
 
 <template>
-    <section class="main__section section gallery-section">
+    <!-- <section class="main__section section gallery-section">
         <div class="gallery-section__container container">
             <div class="gallery-section__text-wrapper">
                 <span class="text-md gallery-section__overline">Share your setup with</span>
@@ -14,6 +18,22 @@ const urls = mozaicImagesUrls
             </div>
             <div class="gallery-section__image-wrapper" v-for="(url, index) in urls" :key="index" :style="{backgroundImage: `url(${getUrlFromString(url.url)})`}"></div>
         </div>
+    </section> -->
+    <section class="main_section section gallery-section-mobile">
+        <div class="gallery-section-mobile__text-wrapper">
+                <span class="text-xl gallery-section-mobile__overline">Share your setup with</span>
+                <h2 class="text-5xl gallery-section-mobile__title">#FuniroFurniture</h2>
+        </div>
+        <swiper 
+        class="gallery-section-mobile__container container"
+        :effect="'cards'"
+        :modules="[EffectCards]"
+        :grab-cursor="true"
+        >
+            <swiper-slide v-for="(url, index) in urls" :key="index" :style="{backgroundImage: `url(${getUrlFromString(url.url)})`}" class="gallery-section-mobile__image-wrapper">
+            </swiper-slide>
+            <div class="swiper-pagination" slot="pagination"></div>
+        </swiper>
     </section>
 </template>
 
@@ -102,6 +122,47 @@ const urls = mozaicImagesUrls
             grid-column: 9 / span 2;
             grid-row: 9 / span 3;
         }
+    }
+}
+
+.gallery-section-mobile {
+    width: 90vw;
+    height: 100vh;
+    padding: 20px;
+    display: flex;
+    flex-direction: column;
+    margin: 0;
+    box-sizing: border-box;
+
+    &__container {
+        border: 1px solid deeppink;
+        padding: 20px;
+        width: 200px;
+        height: 400px;
+    }
+
+    &__image-wrapper {
+        background-size: cover;
+        background-position: center;
+        border-radius: 20px;
+    }
+
+    &__text-wrapper {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        text-align: center;
+        gap: 8px;
+        height: 60px;
+    }
+
+    &__overline {
+        font-weight: 500;
+        color: var(--color-granite-gray);
+    }
+
+    &__title {
+        color: var(--color-dark-charcoal);
     }
 }
 </style>
