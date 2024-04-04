52f0f180d1bff9d4c79838988094f3ba43576860
YourBestDream <ferari100w3@gmail.com>
2024-04-04 10:13:39 +0300

    Navbar kinda responsive

--- src/components/shared/PageHeader.vue
+++ src/components/shared/PageHeader.vue
@@ -1,9 +1,25 @@
 <script setup>
+import {onMounted, onUnmounted, ref, computed} from 'vue';
 import HeaderLogo from './HeaderLogo.vue'
 import HeaderNavigation from './HeaderNavigation.vue';
 import GenericLink from '../generics/GenericLink.vue';
 import CartIcon from '../../assets/icons/CartIcon.svg';
 import UserIcon from '../../assets/icons/UserIcon.svg';
+
+const mediaFlag = ref(window.innerWidth);
+const isMenuVisible = computed(() => mediaFlag.value >= 768);
+
+const updateMediaFlag = () => {
+  mediaFlag.value = window.innerWidth;
+};
+
+onMounted(() => {
+  window.addEventListener('resize', updateMediaFlag);
+});
+
+onUnmounted(() => {
+  window.removeEventListener('resize', updateMediaFlag);
+});
 </script>
 
 <template>
@@ -11,9 +27,9 @@ import UserIcon from '../../assets/icons/UserIcon.svg';
         <div class="header__container container">
             <HeaderLogo />
 
-            <HeaderNavigation />
+            <HeaderNavigation v-show="isMenuVisible"/>
 
-            <div class="header__link-wrapper">
+            <div class="header__link-wrapper" v-show="isMenuVisible">
                 <GenericLink href="#" containerClass="header__link">
                     <CartIcon />
                 </GenericLink>
@@ -22,24 +38,35 @@ import UserIcon from '../../assets/icons/UserIcon.svg';
                     <UserIcon />
                 </GenericLink>
             </div>
+            <span class="header__container-toggle"></span>
         </div>
     </header>
 </template>
 
 <style lang="scss" scoped>
-.header__container {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    width: 100%;
-    max-width: 1380px;
-    padding: 30px 0;
-    margin: 0 auto;
-}
-
-.header__link-wrapper {
-    display: flex;
-    align-items: center;
-    column-gap: 24px;
+.header{
+    &__container{
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        width: 100%;
+        max-width: 1380px;
+        padding: 30px 0;
+        margin: 0 auto;
+    }
+    &__link-wrapper{
+        display: flex;
+        align-items: center;
+        column-gap: 24px;
+    }
+    &__container-toggle{
+        display:none;
+    }
+    @media (max-width:768px) {
+        &__container-toggle{
+            display: flex;
+            justify-content: flex-end;
+        }
+    }
 }
 </style>
