f2f46897b996255e8e712de329ce123354f168a5
AlmightyCrickityCrick <s02linamd@gmail.com>
2024-04-04 10:18:25 +0300

    Modified a bit the hero card to be box model appropriate. Changed sections from App to HomeView.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,15 @@
 <script setup>
+import HeroSection from '../components/home/HeroSection.vue'
+import CategorySection from '../components/home/CategorySection.vue'
+import ProductSection from '../components/home/ProductSection.vue'
+import MozaicComponent from '../components/home/MozaicComponent.vue'
 </script>
 
 <template>
     <main class="main">
+        <HeroSection />
+        <CategorySection />
+        <ProductSection />
+        <MozaicComponent />
     </main>
 </template>
