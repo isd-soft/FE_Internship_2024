508b0c9d0464e2feb78b480153dd5781aaa69a88
YourBestDream <ferari100w3@gmail.com>
2024-04-04 12:44:20 +0300

    Added toggle to show the dropdown menu

--- src/components/shared/PageHeader.vue
+++ src/components/shared/PageHeader.vue
@@ -1,19 +1,20 @@
 <script setup>
 import {onMounted, onUnmounted, ref, computed} from 'vue';
-import {mediaFlag, isMenuVisible, updateMediaFlag} from '../../utils/pageHeaderHelperFuncs.js'
+import {mediaFlag, isMenuVisible, updateMediaFlag, isCollapsedMenuOpen, toggleCollapsedMenu} from '../../utils/pageHeaderHelperFuncs.js'
 import HeaderLogo from './HeaderLogo.vue'
 import HeaderNavigation from './HeaderNavigation.vue';
 import GenericLink from '../generics/GenericLink.vue';
 import CartIcon from '../../assets/icons/CartIcon.svg';
 import UserIcon from '../../assets/icons/UserIcon.svg';
+import HamburgerMenuIcon from '../../assets/icons/HamburgerMenuIcon.svg'
 
 onMounted(() => {
   window.addEventListener('resize', updateMediaFlag);
-});
+})
 
 onUnmounted(() => {
   window.removeEventListener('resize', updateMediaFlag);
-});
+})
 </script>
 
 <template>
@@ -24,7 +25,7 @@ onUnmounted(() => {
             <HeaderNavigation v-show="isMenuVisible"/>
 
             <div class="header__link-wrapper" v-show="isMenuVisible">
-                <GenericLink href="#" containerClass="header__link">
+                <GenericLink href="/cart" containerClass="header__link">
                     <CartIcon />
                 </GenericLink>
 
@@ -32,7 +33,16 @@ onUnmounted(() => {
                     <UserIcon />
                 </GenericLink>
             </div>
-            <span class="header__container-toggle"></span>
+            <span class="header__container-toggle">
+                <HamburgerMenuIcon />
+            </span>
+            <!-- <span class="header__container-toggle mod model-5">
+                <div class="menu">
+                    <div class="bar"></div>
+                    <div class="bar"></div>
+                    <div class="bar"></div>
+                </div>
+            </span> -->
         </div>
     </header>
 </template>
@@ -60,7 +70,51 @@ onUnmounted(() => {
         &__container-toggle{
             display: flex;
             justify-content: flex-end;
+            align-items: center;
+            .hamburger{
+                height: calc(0.5*100px);
+                width: auto;
+                // transform: scaleY(calc(0.52 * 100px));
+                // transform-origin: center;
+            }
         }
     }
 }
+// .header__container-toggle.mod.model-5 {
+//   background-color: #F2276C;
+// }
+// .menu {
+//   cursor: pointer;
+//   position: relative;
+//   color: black;
+//   transition: transform 0.3s ease 0.5s;
+// }
+// .bar {
+//   position: absolute;
+//   transition: top 0.3s ease 0.3s, transform 0.3s ease-out 0.1s;
+//   &:nth-of-type(1) { top: 15px; }
+//   &:nth-of-type(2) { top: 30px; transition: 0.3s ease 0.3s; }
+//   &:nth-of-type(3) { top: 45px; }
+// }
+// .menu:hover {
+//   transform: rotate(45deg);
+//   .bar {
+//     display: block;
+//     width: 30px; /* Example width */
+//     height: 4px; /* Example height */
+//     background-color: #fff; /* Example color, ensuring visibility */
+//     margin: 6px auto;
+//     &:nth-of-type(1) { 
+//       top: 30px; 
+//       transform: rotate(0deg);
+//       transition: top 0.3s ease 0.1s, transform 0.3s ease-out 0.5s;
+//     }
+//     &:nth-of-type(2) { opacity: 0; }
+//     &:nth-of-type(3) { 
+//       top: 30px; 
+//       transform: rotate(90deg);
+//       transition: top 0.3s ease 0.1s, transform 0.3s ease-out 0.7s;
+//     }
+//   }
+// }
 </style>
--- src/utils/pageHeaderHelperFuncs.js
+++ src/utils/pageHeaderHelperFuncs.js
@@ -1,6 +1,15 @@
-export const mediaFlag = ref(window.innerWidth);
-export const isMenuVisible = computed(() => mediaFlag.value >= 768);
+import {ref, computed} from 'vue'
 
+export const mediaFlag = ref(window.innerWidth) // Checks the width of the viewport
+export const isMenuVisible = computed(() => mediaFlag.value > 768) // Sets the visibility of non-collapsable menu
+export const isCollapsedMenuOpen = ref(false) // Checks if the collapsible menu open
+
+// Updates the values of the mediaFlag so it doesn't stay the same
 export const updateMediaFlag = () => {
     mediaFlag.value = window.innerWidth;
-  };
+  }
+
+// Changes the value of isCollapsedMenuOpen on the opposite
+export const toggleCollapsedMenu = () => {
+    isCollapsedMenuOpen.value = !isCollapsedMenuOpen.value
+}
