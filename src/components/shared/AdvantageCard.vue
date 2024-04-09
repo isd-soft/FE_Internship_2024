1995d9399b73348966bed49fdb1cfa6556f109d9
Stanislav Prutean <stanislav.prutean@iis.utm.md>
2024-04-09 10:12:26 +0300

    make advantageSection responsive

--- src/components/shared/AdvantageCard.vue
+++ src/components/shared/AdvantageCard.vue
@@ -9,18 +9,18 @@ const props=defineProps({
         type: String,
         default: ''
     },
-    description:{
+    description: {
         type: String,
         default: ''
-    }
+    },
 })
 </script>
 <template>
     <div class="AdvantageCard">
         <component :is="icon"/>
         <div class="AdvantageCard__text">
-        <h1 class="AdvantageCard__h1 text-lg">{{ props.name }}</h1>
-        <p class="AdvantageCard__p text-md">{{ props.description }}</p>   
+        <h1 class="AdvantageCard__title text-lg">{{ name }}</h1>
+        <p class="AdvantageCard__description text-md">{{ description }}</p>
         </div>
     </div>
 </template>
@@ -29,11 +29,11 @@ const props=defineProps({
     display: flex;
     align-items: center;
     gap: .75rem;
-    &__h1{
+    &__title{
         font-weight: 600;
         margin:0;
     }
-    &__p{
+    &__description{
         font-weight: 500;
         margin: 0;
         color: var(--color-taupe-gray);
