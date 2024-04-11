b6be42ac97a38e7734fbde531aecba9d6dafa3b4
AlmightyCrickityCrick <s02linamd@gmail.com>
2024-04-11 09:20:49 +0300

    create default view and default view route

---
+++ src/views/DefaultView.vue
@@ -0,0 +1,14 @@
+<script setup>
+import PageHeader from '../components/shared/PageHeader.vue'
+import PageFooter from '../components/shared/PageFooter.vue'
+</script>
+<template>
+    <div>
+    <PageHeader />
+    <RouterView />
+    <PageFooter />
+  </div> 
+</template>
+
+<style>
+</style>
